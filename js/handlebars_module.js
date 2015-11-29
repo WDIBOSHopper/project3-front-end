fba4dac45264067d5829bd13f97ff663f1ae0a74
bbarber <bbarber@coastalcomputerservices.net>
2015-11-29 09:16:07 -0500

    Added Handlebars functionality for Dashboard page.  Both Blogs summary section and Recent Blog posts sections are being populated by handlebars.  Also removed a lot of unused code.
    Updated handlebars_module for dummy blog list data (used same blog posts as used
    on 0ne_page_wonder page).  Also used bhHandlers code to popular posts section of
    Dashboard page.
    
    Remaining issue with Blogs Summary section of page --->  lost dividing line
    between each entry when code was moved between {{#each}} & {{/each}} tags.
    
    Next items remaining for Dashboard are to tie in logout functionality, tie the
    media section to assets/images folder, and begin javascript coding for button
    event handlers, of which there are many.

--- js/handlebars_module.js
+++ js/handlebars_module.js
@@ -1,5 +1,7 @@
 var data = {posts: [ {blogTitle: "Lauren's Blog", title: "My first post", content: "I posted a thing!", date: "2015-10-15"}, {blogTitle: "Bill's Blog",title: "My second post", content: "I posted a thing!", date: "2015-10-15"},{blogTitle: "Rachel's blog", title: "My third post", content: "I posted a thing!", date: "2015-10-15"}]};
 
+var blogData = {blogs: [ {blogName: "My Biking Passion", name: "My first blog", details: "I created a blog!", date: "2015-10-15"}, {blogName: "My Biking Passion", name: "Essential Biking Safety Gear", details: "I created a blog!", date: "2015-10-15"},{blogName: "Rachel's blog", name: "My third blog", details: "I created a thing!", date: "2015-10-15"}]};
+
 
 var bhHandlebars = {
   displayHomepage: function(data){
@@ -7,7 +9,18 @@ var bhHandlebars = {
     var newHTML = blogPostsTemplate(data);
     $("#putBlogPosts").html(newHTML);
   }
+};
 
+var blogHandlebars = {
+ displayDashboard: function(blogData){
+    var blogsTemplate = Handlebars.compile($('#blogLists').html());
+    var newHTML = blogsTemplate(blogData);
+    $("#putBlogLists").html(newHTML);
+  }
 };
 
 bhHandlebars.displayHomepage(data);
+
+blogHandlebars.displayDashboard(blogData);
+
+
