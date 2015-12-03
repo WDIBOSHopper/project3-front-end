2d53cc9d272e801497cc3a5a78e65680a4a364d7
Lauren Grant <lgrant1286@gmail.com>
2015-12-03 14:20:38 -0500

    Creates global data variable to store userName, UserId and PageID.
    * Tested on front end and it works.
    * Pair programming RTS, LG, BB.

--- js/bh_api.js
+++ js/bh_api.js
@@ -28,6 +28,9 @@ var fakePostUpdateData = {
 
  };
 
+ // fake data ends HERE
+var userData = {userName: null, userPage: null, userId: null};
+
 var bhHelpers = {
   wrap: function (root, formData) {
     var wrapper = {};
@@ -222,11 +225,14 @@ $(document).ready(function(){
         }, 3000);
         $('#successAlert').hide();
         console.log(data.user._id);
+        userData.userName = data.user.userName;
+        userData.userId = data.user._id;
         console.log(data);
         bhApi.createPage({title: "My Blog", content: "A blog for me.", url: data.user.userName,  owner: data.user._id}, function(err, data){
             if (err){
             console.error(err);
             } else {
+              userData.userPage = data.page._id;
             console.log('You created a post. FOR REALS.', data);
             }
         });
