0bcfed1ee9fd03297cbb926ac6f99a302e5378ed
racheltstevens <racheltstevens@gmail.com>
2016-01-08 12:04:39 -0500

    Fixes registration
    * Easy to click on register link
    * Succss alert shows on success
    * Please wait alert shows while registration happens

--- js/registration-form.js
+++ js/registration-form.js
@@ -7,20 +7,16 @@ $(document).ready(function(){
   $('#login-form').on('submit', function(e){
     e.preventDefault();
     var data = $(this).serialize();
-    console.log(data);
     bhApi.login(data, function (err, data){
       if (err){
         console.error(err);
         return false;
       }
-        console.log('login response data ' + data);
         userData.userName = data.userName;
         userData.userId = data._id;
         bhApi.dashboard(function(err, data){
           if(err){console.error}
           else{
-            console.log("this data is passed to handlebars");
-            console.log(data);
 
             bhHandlebars.displayDashboard(data);
             dashboardHandlers();
@@ -34,22 +30,24 @@ $(document).ready(function(){
   $('#registration-form').on('submit', function(e){
     e.preventDefault();
     var data = $(this).serialize();
-    console.log(data);
+    $("#wait").show();
     bhApi.register(data, function (err, data){
       if (err){
         console.error(err);
+        $("#wait").hide();
         $('#errorAlert').show();
         setTimeout(function(){
           $('#errorAlert').hide();
         }, 3000);
       } else {
+      $("#wait").hide();  
       $('#successAlert').show();
        setTimeout(function(){
         $('#registration-div').hide();
-        }, 3000);
         $('#successAlert').hide();
-        console.log(data.user._id);
-        console.log(data);
+        }, 3000);
+        
+  
         bhApi.createPage({title: "My Blog", content: "A blog for me.", url: data.user.userName,  owner: data.user._id}, function(err, data){
             if (err){
             console.error(err);
